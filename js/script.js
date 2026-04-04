d1efea30d099db2d6711ea2849583d23036df883
diogohrq <69657046+diogohrq@users.noreply.github.com>
2026-04-04 19:38:32 -0300

    Create structure and operations functionality

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let number1 = '';
+let number2 = '';
+let operator = null;
+
+const operate = (num1, num2, operator) => {
+  switch (operator) {
+    case 'add':
+      return add(num1, num2);
+    case 'subtract':
+      return subtract(num1, num2);
+    case 'multiply':
+      return multiply(num1, num2);
+    case 'divide':
+      return divide(num1, num2)
+    default:
+      return null;
+  }
+}
+
+const add = (num1, num2) => num1 + num2;
+
+const subtract = (num1, num2) => num1 - num2;
+
+const multiply = (num1, num2) => num1 * num2;
+
+const divide = (num1, num2) => num1 / num2;
